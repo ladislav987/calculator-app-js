c8296dfec2999bbc7299fe356df9b5e30e3ab682
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-02-09 13:52:31 +0100

    add toogle function for style btn

--- js/themeHandler.js
+++ js/themeHandler.js
@@ -1,6 +1,10 @@
 const themeButton = document.getElementById("themeBtn");
 const body = document.getElementById("body");
 
+const prvy = document.getElementById("oneIne");
+const druhy = document.getElementById("twoIne");
+const treti = document.getElementById("threeIne");
+
 var themeClass = window.localStorage.getItem("theme") || window.localStorage.setItem("theme", "theme-1");
 
 
@@ -8,8 +12,12 @@ var themeClass = window.localStorage.getItem("theme") || window.localStorage.set
 if (document.getElementById("body").className == "") {
     if (themeClass == undefined) {
         body.classList.add("theme-1");
+        prvy.checked = true;
     }
-    else body.classList.add(themeClass);
+    else {
+        body.classList.add(themeClass);
+        radioBtnCheckSame();
+    }
 }
 
 
@@ -26,12 +34,15 @@ function themeHandler() {
 
     if (themeClass === "theme-1") {
         themeSet = "theme-2";
+        druhy.checked = true;
     }
     else if (themeClass === "theme-2") {
         themeSet= "theme-3";
+        treti.checked = true;
     }
     else if (themeClass === "theme-3") {
         themeSet= "theme-1";
+        prvy.checked = true;
     }
 
 
@@ -40,4 +51,16 @@ function themeHandler() {
     body.classList.remove(bodyOldClass);
     body.classList.add(themeSet);
 
+}
+
+function radioBtnCheckSame() {
+    if (themeClass === "theme-1") {
+        prvy.checked = true;
+    }
+    else if (themeClass === "theme-2") {
+        druhy.checked = true;
+    }
+    else if (themeClass === "theme-3") {
+        treti.checked = true;
+    }
 }
