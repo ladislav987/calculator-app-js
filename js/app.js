07d550d68e65c1650eed297bad0e5fb92365806f
Ladislav Chvastaš <ladislav.chvastas@student.tuke.sk>
2022-02-09 12:37:16 +0100

    refactor code,

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ const buttonsContainer = document.getElementsByClassName("buttonsContainer")[0];
 var inputNumber = " ";
 var firstNumbers = 0;
 var secondNumbers = 0;
-var vysledok = 0;
-var znamnienkoHore = "";
+var result = 0;
+var sign = "";
 
 var firstTime = true;
 
@@ -57,22 +57,22 @@ buttonsContainer.addEventListener('click', event => {
             break;
 
         case "addition":
-            operaciaZnamienka(inputNumber, "plus");
+            operaciaZnamienka(inputNumber, "addition");
 
             break;
 
         case "subtraction":
-            operaciaZnamienka(inputNumber, "minus");
+            operaciaZnamienka(inputNumber, "subtraction");
             clearInput();
             break;
 
         case "multiplication":
-            operaciaZnamienka(inputNumber, "krat");
+            operaciaZnamienka(inputNumber, "multiplication");
             clearInput();
             break;
 
         case "division":
-            operaciaZnamienka(inputNumber, "delene");
+            operaciaZnamienka(inputNumber, "division");
             clearInput();
             break;
 
@@ -96,27 +96,13 @@ buttonsContainer.addEventListener('click', event => {
 // //? Function section
 
 function addNumber(number) {
-    // console.log(" začatok addNumber funkcie");
-
     inputNumber += number;
     document.getElementById("amountInput").innerHTML = inputNumber;
-
-    // console.log("Input number: ", inputNumber);
-    // console.log("First number: ", firstNumbers);
-    // console.log("Second number: ", secondNumbers);
-    // console.log("firstTime: ", firstTime);
-    // console.log("vysledok: ", vysledok);
-    // console.log("znamienko: ", znamnienkoHore);
-
-    // console.log(" koniec addNumber funkcie");
-    // console.log(" ");
 }
 
 function operaciaZnamienka(number, znamienko) {
-    // console.log(" začatok operaciaZnamienka funkcie");
-
     if (znamienko != undefined) {
-        znamnienkoHore = znamienko;
+        sign = znamienko;
     }
 
     if (firstTime) {
@@ -128,405 +114,61 @@ function operaciaZnamienka(number, znamienko) {
         secondNumbers = Number(number);
         clearInput();
     }
-
-
-
-    // console.log("Input number: ", inputNumber);
-    // console.log("First number: ", firstNumbers);
-    // console.log("Second number: ", secondNumbers);
-    // console.log("firstTime: ", firstTime);
-    // console.log("vysledok: ", vysledok);
-    // console.log("znamienko: ", znamnienkoHore);
-
-    // console.log(" koniec operaciaZnamienka funkcie");
-    // console.log(" ");
 }
 
 
 
 function spocitaj() {
-    // console.log(" začatok spocitaj funkcie");
-
     if (secondNumbers == 0) {
         operaciaZnamienka(inputNumber);
     }
 
     if (1) {
-        if (znamnienkoHore === "plus") {
-            vysledok = firstNumbers + secondNumbers;
+        if (sign === "addition") {
+            result = firstNumbers + secondNumbers;
         }
-        if (znamnienkoHore === "minus") {
-            vysledok = firstNumbers - secondNumbers;
+        if (sign === "subtraction") {
+            result = firstNumbers - secondNumbers;
         }
-        if (znamnienkoHore === "krat") {
-            vysledok = firstNumbers * secondNumbers;
+        if (sign === "multiplication") {
+            result = firstNumbers * secondNumbers;
         }
-        if (znamnienkoHore === "delene") {
-            vysledok = firstNumbers / secondNumbers;
+        if (sign === "division") {
+            result = firstNumbers / secondNumbers;
         }
     }
-    document.getElementById("amountOutput").innerHTML = vysledok;
-
-    // console.log("Input number: ", inputNumber);
-    // console.log("First number: ", firstNumbers);
-    // console.log("Second number: ", secondNumbers);
-    // console.log("firstTime: ", firstTime);
-    // console.log("vysledok: ", vysledok);
-    // console.log("znamienko: ", znamnienkoHore);
-    // console.log(" ");
-
-    // console.log(" koniec spocitaj funkcie");
+    document.getElementById("amountOutput").innerHTML = result;
 
     resetCalculator();
 }
 
 
 function clearInput() {
-    // console.log(" začiatok clearInput funkcie");
-
     document.getElementById("amountInput").innerHTML = 0;
     inputNumber = "";
-
-    // console.log(" koniec clearInput funkcie");
-    // console.log(" ");
 }
 
 
 
 function resetCalculator() {
-    // console.log(" začiatok resetCalculator funkcie");
-
     inputNumber = "";
     firstNumbers = 0;
     secondNumbers = 0;
-    znamnienkoHore = "";
+    sign = "";
     firstTime = true;
 
-    // console.log("Input number: ", inputNumber);
-    // console.log("First number: ", firstNumbers);
-    // console.log("Second number: ", secondNumbers);
-    // console.log("firstTime: ", firstTime);
-    // console.log("vysledok: ", vysledok);
-    // console.log("znamienko: ", znamnienkoHore);
-    // console.log(" ");
-
-
-    // console.log(" koniec resetCalculator funkcie");
 }
 
 function resetVysledok() {
     document.getElementById("amountOutput").innerHTML = 0;
-    vysledok = 0;
+    result = 0;
 }
 
 function deleteLastCharacter() {
-    let result = inputNumber.slice(0 , -1);
+    let result = inputNumber.slice(0, -1);
     inputNumber = result;
     document.getElementById("amountInput").innerHTML = inputNumber;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //! Scénar 1
-
-// // 1. po stlačení tlačitka sa vola funckia addNumber() ktorá do inputu napíše číslo
-// // 2. stačím tlačitko operácie, volá sa funkcia operaciaZnamienka() ktorá pozera či je už zadané znamienko a vynuluje sa input
-// // 3. zapíšem druhé číslo
-// // 4. stlačím rovná sa
-
-// //! Scénar 2
-
-// // 1. prvé stlačim rovná sa
-
-
-// //? pridáva čisla do inputu aby sa vykreslili
-
-// function addNumber(number) {
-//     console.log(" začatok addNumber funkcie");
-
-//     inputNumber += number;
-//     document.getElementById("amountInput").innerHTML = inputNumber;
-
-//     console.log(" koniec addNumber funkcie");
-// }
-
-// //? pokiaľ je definované znamienko pridá ho do premennej
-// //? kontroluje či je zadavane prvé číslo
-// function operaciaZnamienka(number, znamienko) {
-//     console.log(" začatok operaciaZnamienka funkcie");
-
-//     if (znamienko != undefined) {
-//         znamnienkoHore = znamienko;
-//     }
-
-//     if (firstTime) {
-//         firstNumbers = Number(number);
-//         firstTime = false;
-//     }
-//     else secondNumbers = Number(number);
-
-//     console.log(" koniec operaciaZnamienka funkcie");
-// }
-
-// function spocitaj() {
-//     console.log(" začatok spocitaj funkcie");
-
-//     let vysledok = 0;
-
-//     if (firstNumbers != 0) {
-//         console.log("firstNumber nieje 0la!!!!!!!!!!!!!!!!!!!!!!!!!!!! ale je:", firstNumbers);
-//         if (znamnienkoHore === "plus") {
-//             vysledok = firstNumbers + secondNumbers;
-//         }
-//         if (znamnienkoHore === "minus") {
-//             vysledok = firstNumbers - secondNumbers;
-//         }
-//         if (znamnienkoHore === "krat") {
-//             vysledok = firstNumbers * secondNumbers;
-//         }
-//         if (znamnienkoHore === "delene") {
-//             vysledok = firstNumbers / secondNumbers;
-//         }
-
-
-//         document.getElementById("amountInput").innerHTML = vysledok;
-//         firstNumbers = vysledok;
-
-
-//     }
-
-//     console.log("Input number: ", inputNumber);
-//     console.log("First number: ", firstNumbers);
-//     console.log("Second number: ", secondNumbers);
-
-//     console.log(" koniec spocitaj funkcie");
-//     // clearInputNumber();
-// }
-
-
-
-// function resetOutput() {
-//     console.log(" začiatok resetOutput funkcie");
-
-//     document.getElementById("amountInput").innerHTML = 0;
-//     inputNumber = "";
-//     firstNumbers = 0;
-
-//     console.log("Input number: ", inputNumber);
-//     console.log("First number: ", firstNumbers);
-//     console.log("Second number: ", secondNumbers);
-
-
-//     console.log(" koniec resetOutput funkcie");
-// }
-
-
-
-
-// function clearInput() {
-//     console.log(" začiatok clearInput funkcie");
-
-//     document.getElementById("amountInput").innerHTML = 0;
-//     inputNumber = "";
-
-//     console.log(" koniec clearInput funkcie");
-// }
-
-// function clearInputNumber() {
-//     console.log(" začiatok clearInputNumber funkcie");
-
-//     inputNumber = "";
-
-//     console.log(" koniec clearInputNumber funkcie");
-
-// }
-
-// // function resetAll() {
-// //     document.getElementById("amountInput").innerHTML = 0;
-// //     inputNumber = "";
-// //     firstNumbers = "";
-// //     secondNumbers = "";
-
-// //     console.log("Input number: ", inputNumber);
-// //     console.log("First number: ", firstNumbers);
-// //     console.log("Second number: ", secondNumbers);
-// // }
-
-
-
-// // function count(number) {
-
-// //     let skuska = "";
-
-// //     skuska = number.split("+");
-
-// //     console.log(skuska[0]);
-// // }
-
-
-
-
-// function addNumber(number) {
-//     console.log(" začatok addNumber funkcie");
-
-//     inputNumber += number;
-//     document.getElementById("amountInput").innerHTML = inputNumber;
-
-//     console.log("Input number: ", inputNumber);
-//     console.log("First number: ", firstNumbers);
-//     console.log("Second number: ", secondNumbers);
-//     console.log("firstTime: ", firstTime);
-//     console.log("vysledok: ", vysledok);
-//     console.log("znamienko: ", znamnienkoHore);
-
-//     console.log(" koniec addNumber funkcie");
-//     console.log(" ");
-// }
-
-// function operaciaZnamienka(number, znamienko) {
-//     console.log(" začatok operaciaZnamienka funkcie");
-
-//     if (znamienko != undefined) {
-//         znamnienkoHore = znamienko;
-//     }
-
-//     if (firstTime) {
-//         firstNumbers = Number(number);
-//         firstTime = false;
-//         clearInput();
-//     }
-//     else {
-//         secondNumbers = Number(number);
-//         spocitaj();
-//         resetCalculator();
-
-//     }
-
-//     clearInput();
-
-
-//     console.log("Input number: ", inputNumber);
-//     console.log("First number: ", firstNumbers);
-//     console.log("Second number: ", secondNumbers);
-//     console.log("firstTime: ", firstTime);
-//     console.log("vysledok: ", vysledok);
-//     console.log("znamienko: ", znamnienkoHore);
-
-//     console.log(" koniec operaciaZnamienka funkcie");
-//     console.log(" ");
-// }
-
-
-
-
-
-// function spocitaj() {
-//     console.log(" začatok spocitaj funkcie");
-
-//     if (firstNumbers != 0) {
-//         console.log("firstNumber nieje 0la!!!!!!!!!!!!!!!!!!!!!!!!!!!! ale je:", firstNumbers);
-//         if (znamnienkoHore === "plus") {
-//             vysledok = firstNumbers + secondNumbers;
-//         }
-//         if (znamnienkoHore === "minus") {
-//             vysledok = firstNumbers - secondNumbers;
-//         }
-//         if (znamnienkoHore === "krat") {
-//             vysledok = firstNumbers * secondNumbers;
-//         }
-//         if (znamnienkoHore === "delene") {
-//             vysledok = firstNumbers / secondNumbers;
-//         }
-
-
-//         document.getElementById("amountOutput").innerHTML = vysledok;
-//         firstNumbers = vysledok;
-
-
-//     }
-
-//     console.log("Input number: ", inputNumber);
-//     console.log("First number: ", firstNumbers);
-//     console.log("Second number: ", secondNumbers);
-//     console.log("firstTime: ", firstTime);
-//     console.log("vysledok: ", vysledok);
-//     console.log("znamienko: ", znamnienkoHore);
-//     console.log(" ");
-
-//     console.log(" koniec spocitaj funkcie");
-//     // clearInputNumber();
-// }
-
-
-// function clearInput() {
-//     console.log(" začiatok clearInput funkcie");
-
-//     document.getElementById("amountInput").innerHTML = 0;
-//     inputNumber = "";
-
-//     console.log(" koniec clearInput funkcie");
-//     console.log(" ");
-// }
-
-// function resetCalculator() {
-//     console.log(" začiatok resetCalculator funkcie");
-
-//     inputNumber = "";
-//     firstNumbers = 0;
-//     secondNumbers = 0;
-//     znamnienkoHore = "";
-//     firstTime= true;
-
-//     console.log("Input number: ", inputNumber);
-//     console.log("First number: ", firstNumbers);
-//     console.log("Second number: ", secondNumbers);
-//     console.log("firstTime: ", firstTime);
-//     console.log("vysledok: ", vysledok);
-//     console.log("znamienko: ", znamnienkoHore);
-//     console.log(" ");
-
-
-//     console.log(" koniec resetCalculator funkcie");
-// }
 
 
 
